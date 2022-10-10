73dfab5aa3ed6dac2efce616044b3b8ae068017b
ashaduzzaman mondal <ashaduzzaman2002.com>
2022-10-11 00:12:33 +0530

    Updating Design

---
+++ js/script.js
@@ -0,0 +1,52 @@
+profile = document.getElementById("profile");
+
+let isLogin = true;
+
+!isLogin
+  ? (profile.innerHTML = `
+<div class="login-btn">
+            <button>Login</button>
+        </div>`)
+  : (profile.innerHTML = `
+        <img src="images/profile3.png" alt="" />`);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000);
+}
